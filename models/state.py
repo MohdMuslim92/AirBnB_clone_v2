cbdcc73693e8c6b39c6874a9513814010aaf95ce
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-09-23 22:03:14 +0300

    Update models/state.py
    - Fix issues with models.storage_t

--- models/state.py
+++ models/state.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os import getenv
-from models.city import City
 
 
-class State(BaseModel, Base):
-    """ The State class which has relationship with the City class """
-    if models.storage_t == "db":
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    class State(BaseModel, Base):
+        """The State class, which has a relationship with the City class when
+        using a database."""
         __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state',
-                          cascade='all, delete, delete-orphan')
-    else:
-        name = ''
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
 
-    def __init__(self, *args, **kwargs):
-        """initialize state"""
-        super().__init__(*args, **kwargs)
+        def __init__(self, *args, **kwargs):
+            """initializes state"""
+            super().__init__(*args, **kwargs)
+else:
+    class State(BaseModel, Base):
+        """The State class, which has a list of related City instances when
+        not using a database."""
+        name = ""
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """Get a list of all related City objects."""
-            from models import storage
+            """getter for list of city instances related to the state"""
             city_list = []
             all_cities = models.storage.all(City)
             for city in all_cities.values():
