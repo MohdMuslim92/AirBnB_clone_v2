88378adff036314dff982d89773ba20235c21c2a
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-09-27 18:29:09 +0000

    Update 7-states_list.py
    - Re-Arrange properties, putting teardown at last
    - Edit module docstring

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
 """
-This module contains a simple Flask web application that displays a list of
-states.
-
-The script starts a Flask web application:
-    - Listening on 0.0.0.0, port 5000
-    - Defines a single route: /states_list
-
-Routes:
-    - /states_list: Display a HTML page containing a list of all State objects
-    present in DBStorage,
-                    sorted by name (A->Z).
-                    Each state is listed with its ID and name.
+    This module contains a simple Flask web application that displays a list of
+    states.
 """
 
 from flask import Flask, render_template
@@ -23,19 +13,23 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def close_storage(exception):
-    """Remove the current SQLAlchemy session."""
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Display a list of states sorted by name."""
+    """
+        Display a list of states sorted by name.
+    """
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda state: state.name)
 
     return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """
+        Remove the current SQLAlchemy session.
+    """
+    storage.close()
 
 
 if __name__ == "__main__":
